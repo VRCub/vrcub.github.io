c50998a18aab4ef454c3ad0fec619ce10b46c642
dream_pep <98459127+dream-pep@users.noreply.github.com>
2025-06-23 23:39:03 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
         <Link
             className="button button--secondary button--lg"
-            to="/docs/category/welcome">
+            to="/docs/UserDocs/welcome">
             立即开始
           </Link>
           <Link
